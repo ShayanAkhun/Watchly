ce2e0f577804ada0f79b4e8467ff089a5be10f69
ShayanAkhun <littelmaster391@gmail.com>
2025-04-25 01:09:07 +0500

    new changes

--- app/(tabs)/Search.tsx
+++ app/(tabs)/Search.tsx
@@ -1,93 +1,127 @@
-import { View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
-import React,{useEffect, useState} from 'react'
-import { images } from '@/constants/images'
-import MovieCard from '@/components/MovieCard'
-import useFetch from '@/services/useFetch'
-import { fetchMovies } from '@/services/api'
-import { icons } from '@/constants/icons'
-import SearchBar from '@/components/SearchBar'
-import { updatesearchCount } from '@/services/appwrite'
+import { useState, useEffect } from "react";
+import { View, Text, ActivityIndicator, FlatList, Image } from "react-native";
+
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 
+import { fetchMovies } from "@/services/api";
 
+
+import SearchBar from "@/components/SearchBar";
+import MovieDisplayCard from "@/components/MovieCard";
+import useFetch from "@/services/useFetch";
+import { updatesearchCount } from "@/services/appwrite";
 
 const Search = () => {
-   const [searchQuery, setSearchQuery] = useState('')
-    const { data: movies,refetch:loadMovies, loading, error ,reset} = useFetch(() => fetchMovies({
-        query: searchQuery
-      }),false)
+  const [searchQuery, setSearchQuery] = useState("");
 
-      useEffect(()=> {
-    updatesearchCount(searchQuery, movies[0])
-        const timeoutId = setTimeout( async() => {
-            if(searchQuery.trim()) {
-                await loadMovies()
-            } else {
-                reset
-            }
-        },500)
-        return ()=> clearTimeout(timeoutId)
-        }, [searchQuery])
+  const {
+    data: movies = [],
+    loading,
+    error,
+    refetch: loadMovies,
+    reset,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+
+  const handleSearch = (text: string) => {
+    setSearchQuery(text);
+  };
+
+  // Debounced search effect
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+
+
+      } else {
+        reset();
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
+  useEffect(()=> {
+   // Call updateSearchCount only if there are results
+   if (movies?.length! > 0 && movies?.[0]) {
+     updatesearchCount(searchQuery, movies[0]);
+  }
+  }, [movies])
+
   return (
-    <View className='bg-primary flex-1'>
-      <Image source={images.bg} className='flex-1 absolute w-full z-0' resizeMode='cover' />
-        <FlatList data={movies}
-            renderItem={({item})=> <MovieCard {...item}/>}
-            keyExtractor={(item) => item.id.toString()}
-            className='px-5'
-            numColumns={3}
-            columnWrapperStyle={{
-                justifyContent: "center",
-                gap: 16,
-                marginVertical:16
-            }}
-            contentContainerStyle={{paddingBottom: 100}}
-            ListHeaderComponent={
-                <>
-                    <View className='w-full flex-row justify-center mt-20 items-center'>
-                        <Image source={icons.logo} className='w-12 h-10'/>
-                    </View>
-                    <View className='my-5'>
-                        <SearchBar
-                        placeholder='Search for movies...'
-                        value={searchQuery}
-                        onChangeText={(text : string)=> setSearchQuery(text)}
-                        />
-                    </View>
-                    {
-                        loading && (
-                            <ActivityIndicator size="large" className='my-3' color="#0000ff"/>
-                        )
-                    }
-                    {
-                        error && (
-                            <Text className='text-red-500 px-5 py-3'>
-                                Error : {error.message}
-                            </Text>
-                        )
-                    }
-                    {
-                        !loading && !error  && searchQuery.trim() &&  movies?.length > 0 &&
-                        (
-                        <Text className='text-xl text-accent font-bold'>
-                            Search results for {" "}
-                            <Text className="text-accent">{searchQuery}</Text>
-                        </Text>)
-                    }
-                </>
-            }
-            ListEmptyComponent={
-                !loading && !error ? (
-                    <View className='mt-10 px-5'>
-                        <Text className='text-center text-gray-500'>
-                            {searchQuery.trim() ? 'No movies found' : 'Search for a movie'}
-                        </Text>
-                    </View>
-                ) : null
-            }
-        />
-    </View>
-  )
-}
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
 
-export default Search
+      <FlatList
+        className="px-5"
+        data={movies as Movie[]}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <MovieDisplayCard {...item} />}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "flex-start",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search for a movie"
+                value={searchQuery}
+                onChangeText={handleSearch}
+              />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+
+            {!loading &&
+              !error &&
+              searchQuery.trim() &&
+              movies?.length! > 0 && (
+                <Text className="text-xl text-white font-bold">
+                  Search Results for{" "}
+                  <Text className="text-accent">{searchQuery}</Text>
+                </Text>
+              )}
+          </>
+        }
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="mt-10 px-5">
+              <Text className="text-center text-gray-500">
+                {searchQuery.trim()
+                  ? "No movies found"
+                  : "Start typing to search for movies"}
+              </Text>
+            </View>
+          ) : null
+        }
+      />
+    </View>
+  );
+};
+
+export default Search;
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,19 +1,57 @@
 
-import {Client, Databases, Query} from "react-native-appwrite"
+import { Client, Databases, ID, Query } from "react-native-appwrite"
 
-const DATABASE_ID=process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTIONS_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTIONS_ID!;
 
 
 const client = new Client()
-.setEndpoint('https://fra.cloud.appwrite.io/v1')
-.setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+    .setEndpoint('https://fra.cloud.appwrite.io/v1')
+    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
 
-const database  = new Databases(client)
+const database = new Databases(client)
 
-export const updatesearchCount = async (query: string, movie : Movie) => {
-    const result = await database.listDocuments(DATABASE_ID, COLLECTIONS_ID,[
-        Query.equal('searchTerm', query)
-    ])
-    console.log(result)
+export const updatesearchCount = async (query: string, movie: Movie) => {
+    try {
+
+        const result = await database.listDocuments(DATABASE_ID, COLLECTIONS_ID, [
+            Query.equal('searchTerm', query)
+        ])
+        if (result.documents.length > 0) {
+            const existingMovies = result.documents[0];
+            await database.updateDocument(
+                DATABASE_ID,
+                COLLECTIONS_ID,
+                existingMovies.$id,
+                {
+                    count: existingMovies.count + 1
+                }
+            )
+
+        } else {
+            await database.createDocument(DATABASE_ID, COLLECTIONS_ID, ID.unique(), {
+                searchTerm: query,
+                movie_id: movie.id,
+                title : movie.title,
+                count: 1,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+
+}
+export const getTrendingMovies = async() : Promise<TrendingMovie[] | undefined> => {
+    try {
+         const result = await database.listDocuments(DATABASE_ID, COLLECTIONS_ID, [
+            Query.limit(5),
+            Query.orderDesc('count')
+        ])
+        return result.documents as unknown as TrendingMovie[]
+    } catch (error) {
+        console.log(error)
+        return undefined
+    }
 }
